2a83a7a4f9f661bf198a8d03af72dd57e4f314d1
AtharvaPaithankar <atharvapaithankar13@gmail.com>
2026-03-03 23:19:48 +0530

    handling production

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,7 +1,7 @@
-import mongoose from "mongoose";
-
 export const connectDB = async () => {
   try {
+    console.log("ENV:", process.env.MONGODB_URI);
+
     const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
